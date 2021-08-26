356dfdd13ba5087d1cbdc6693b22e9a2d1527475
haddockdaniel <43544084+haddockdaniel@users.noreply.github.com>
2021-08-26 13:16:26 -0400

    Initial commit

---
+++ JurisUtilityBase/UtilityBaseMain.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace JurisUtilityBase
+{
+    partial class UtilityBaseMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UtilityBaseMain));
+            this.JurisLogoImageBox = new System.Windows.Forms.PictureBox();
+            this.LexisNexisLogoPictureBox = new System.Windows.Forms.PictureBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.OpenFileDialogOpen = new System.Windows.Forms.OpenFileDialog();
+            this.buttonReport = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxDB = new System.Windows.Forms.TextBox();
+            this.textBoxActCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.JurisLogoImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LexisNexisLogoPictureBox)).BeginInit();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // JurisLogoImageBox
+            // 
+            this.JurisLogoImageBox.Image = ((System.Drawing.Image)(resources.GetObject("JurisLogoImageBox.Image")));
+            this.JurisLogoImageBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("JurisLogoImageBox.InitialImage")));
+            this.JurisLogoImageBox.Location = new System.Drawing.Point(0, 1);
+            this.JurisLogoImageBox.Name = "JurisLogoImageBox";
+            this.JurisLogoImageBox.Size = new System.Drawing.Size(104, 336);
+            this.JurisLogoImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.JurisLogoImageBox.TabIndex = 0;
+            this.JurisLogoImageBox.TabStop = false;
+            // 
+            // LexisNexisLogoPictureBox
+            // 
+            this.LexisNexisLogoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("LexisNexisLogoPictureBox.Image")));
+            this.LexisNexisLogoPictureBox.Location = new System.Drawing.Point(550, 16);
+            this.LexisNexisLogoPictureBox.Name = "LexisNexisLogoPictureBox";
+            this.LexisNexisLogoPictureBox.Size = new System.Drawing.Size(96, 28);
+            this.LexisNexisLogoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.LexisNexisLogoPictureBox.TabIndex = 1;
+            this.LexisNexisLogoPictureBox.TabStop = false;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 345);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(658, 22);
+            this.statusStrip.TabIndex = 2;
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(135, 17);
+            this.toolStripStatusLabel.Text = "Status: Ready to Execute";
+            // 
+            // OpenFileDialogOpen
+            // 
+            this.OpenFileDialogOpen.FileName = "openFileDialog1";
+            // 
+            // buttonReport
+            // 
+            this.buttonReport.BackColor = System.Drawing.Color.LightGray;
+            this.buttonReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReport.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.buttonReport.Location = new System.Drawing.Point(139, 299);
+            this.buttonReport.Name = "buttonReport";
+            this.buttonReport.Size = new System.Drawing.Size(148, 38);
+            this.buttonReport.TabIndex = 16;
+            this.buttonReport.Text = "Exit";
+            this.buttonReport.UseVisualStyleBackColor = false;
+            this.buttonReport.Click += new System.EventHandler(this.buttonReport_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.button1.Location = new System.Drawing.Point(446, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 38);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Database Name (Example: 5073-001)";
+            // 
+            // textBoxDB
+            // 
+            this.textBoxDB.Location = new System.Drawing.Point(126, 47);
+            this.textBoxDB.Name = "textBoxDB";
+            this.textBoxDB.Size = new System.Drawing.Size(313, 20);
+            this.textBoxDB.TabIndex = 19;
+            // 
+            // textBoxActCode
+            // 
+            this.textBoxActCode.Location = new System.Drawing.Point(126, 107);
+            this.textBoxActCode.Name = "textBoxActCode";
+            this.textBoxActCode.Size = new System.Drawing.Size(313, 20);
+            this.textBoxActCode.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Activation Code to Send to Customer";
+            // 
+            // UtilityBaseMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(658, 367);
+            this.Controls.Add(this.textBoxActCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxDB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonReport);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.LexisNexisLogoPictureBox);
+            this.Controls.Add(this.JurisLogoImageBox);
+            this.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UtilityBaseMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Professional Services Activation Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.JurisLogoImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LexisNexisLogoPictureBox)).EndInit();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox JurisLogoImageBox;
+        private System.Windows.Forms.PictureBox LexisNexisLogoPictureBox;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialogOpen;
+        private System.Windows.Forms.Button buttonReport;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxDB;
+        private System.Windows.Forms.TextBox textBoxActCode;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
